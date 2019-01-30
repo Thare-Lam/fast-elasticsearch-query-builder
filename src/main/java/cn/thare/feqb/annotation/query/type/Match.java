f9d96754773e63065857963ac27d4a39c6a6f871
Thare <thare.liang@gmail.com>
2019-01-30 15:44:19 +0800

    remove some comment

--- src/main/java/cn/thare/feqb/annotation/query/type/Match.java
+++ src/main/java/cn/thare/feqb/annotation/query/type/Match.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Field's type must be {@link String}
- */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Match {
